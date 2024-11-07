1a88a6a3591c6bf7c207a9668d7b9cbc8721be19
Muhammad Haleem <muhammad.haleem@muslimhands.ngo>
2024-11-07 15:24:29 +0500

    Final changes in Home page

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -2,14 +2,18 @@ import React, { useEffect, useState, memo } from "react";
 import { ListGroup, OverlayTrigger, Popover, Image } from "react-bootstrap";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import { Link } from "react-router-dom";
+import { Grid, View, useTheme, SelectField , TextField, Text} from '@aws-amplify/ui-react';
+
 import democraticLogo from "../assets/logo/democraticLogo.png";
 import republicanLogo from "../assets/logo/republicanLogo.png";
 import independentLogo from "../assets/logo/independentLogo.jpg";
 
+
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 const API_KEY = "Y26ZeFbtXnbkvefS4HtK1Fc9nCilHguGB3ajrE3L"; // Replace with your actual API key
 
-const MapChart = ({ electionCycle, office, terms }) => {
+const MapChart = ({ electionCycle, office, terms, state, party }) => {
+    const { tokens } = useTheme();
     const [candidates, setCandidates] = useState([]);
     const [allCandidates, setAllCandidates] = useState([]);
     const [clickedCity, setClickedCity] = useState("");
@@ -37,6 +41,14 @@ const MapChart = ({ electionCycle, office, terms }) => {
             if (searchDelay && searchDelay.trim()) {
                 endpoint += `&q=${encodeURIComponent(searchDelay)}`;
             }
+
+            if (state && state.trim()) {
+                endpoint += `&state=${encodeURIComponent(state)}`;
+            }
+
+            if (party && party.trim()) {
+                endpoint += `&party=${encodeURIComponent(party)}`;
+            }
     
             try {
                 const response = await fetch(endpoint);
@@ -53,7 +65,7 @@ const MapChart = ({ electionCycle, office, terms }) => {
         };
     
         fetchCandidates();
-    }, [startYear, office, searchDelay]);
+    }, [startYear, office, searchDelay, state, party]);
 
     // If the searchDelay is cleared, reset candidates to the full list
     useEffect(() => {
@@ -126,78 +138,125 @@ const MapChart = ({ electionCycle, office, terms }) => {
         setClickedCity(geo.properties.name);
     };
 
+     // Count candidates by party
+     const democraticCandidatesCount = candidates.filter(candidate => candidate.party === 'DEM').length;
+     const republicanCandidatesCount = candidates.filter(candidate => candidate.party === 'REP').length;
+ 
+
     return (
       <div data-tip="" id="keepinside">
-          <ComposableMap projection="geoAlbersUsa">
-              <Geographies geography={geoUrl}>
-                  {({ geographies }) =>
-                      geographies.map((geo, index) => {
-                          // Convert state name to abbreviation
-                          const stateAbbreviation = stateAbbreviations[geo.properties.name];
-                          
-                          // Find the first candidate for this state
-                          const candidate = candidates.find(
-                              (candidate) => candidate.state === stateAbbreviation
-                          );
-                          const party = candidate ? candidate.party : null;
-  
-                          // Set color based on party
-                          let partyColor = "#DCDCDC"; // Default color if no candidates
+            <View display="flex" justifyContent="center" alignItems="center" marginTop={tokens.space.small} padding={tokens.space.small}>
 
-                          if (candidate) {
-                              if (party === "DEM") {
-                                  partyColor = "#0000FF"; // Blue for Democratic party
-                              } else if (party === "REP") {
-                                  partyColor = "#FF0000"; // Red for Republican party
-                              } else {
-                                  partyColor = "#778899"; // Color if candidates exist but not in parties
-                              }
-                          } else {
-                              partyColor = "#DCDCDC"; // Color if no candidates
-                          }
-                          
-  
-                          const isClicked = clickedCity === geo.properties.name;
-  
-                          return (
-                              <OverlayTrigger
-                                  key={index}
-                                  trigger="click"
-                                  rootClose
-                                  placement="right"
-                                  overlay={popover(geo)}
-                              >
-                                  <Geography
-                                      key={geo.rsmKey}
-                                      geography={geo}
-                                      fill={isClicked ? partyColor : "#C0C0C0"}
-                                      onClick={() => handleClick(geo)}
-                                      style={{
-                                          default: {
-                                              fill: partyColor,
-                                              outline: "#101010",
-                                              stroke: "#607D8B",
-                                              strokeWidth: 1,
-                                          },
-                                          hover: {
-                                              fill: partyColor,
-                                              outline: "#101010",
-                                              stroke: "#607D8B",
-                                              strokeWidth: 1,
-                                          },
-                                          pressed: {
-                                              outline: "#101010",
-                                              stroke: "#607D8B",
-                                              strokeWidth: 1,
-                                          },
-                                      }}
-                                  />
-                              </OverlayTrigger>
-                          );
-                      })
-                  }
-              </Geographies>
-          </ComposableMap>
+                {/* Left Candidate (Democratic) */}
+                <View display="flex" flexDirection="column" alignItems="center" padding={tokens.space.large}>
+                    <View 
+                    style={{ width: '10px', height: '100%', backgroundColor: 'blue', marginRight: '15px' }} // Left streak line
+                    />
+                    <Image src={democraticLogo} alt="Democratic Candidate" width="40px" />
+                    <Text fontSize="large" fontWeight="bold" color="blue.600">{democraticCandidatesCount} &nbsp;</Text>
+                    <Text color="blue" fontWeight="bold"> Democratic</Text>
+                </View>
+
+                {/* Middle Section with Circle */}
+                <View display="row" justifyContent="center" alignItems="center" textAlign="center" padding={tokens.space.large}>
+                    <View 
+                    style={{ 
+                        width: '70px', 
+                        height: '70px', 
+                        borderRadius: '50%', 
+                        backgroundColor: '#f0f0f0', 
+                        display: 'flex', 
+                        justifyContent: 'center', 
+                        alignItems: 'center', 
+                        border: '2px solid #000' 
+                    }}
+                    >
+                    <Text fontSize="xl" fontWeight="bold">{candidates.length}</Text>
+                    </View>
+                    <Text fontSize="md" justifyContent="center" fontWeight="bold">Total</Text>
+                    
+                </View>
+
+                {/* Right Candidate (Republican) */}
+                <View display="flex" flexDirection="column" alignItems="center" padding={tokens.space.large}>
+                    <View 
+                    style={{ width: '10px', height: '100%', backgroundColor: 'red', marginLeft: '15px' }} // Right streak line
+                    />
+                    <Image src={republicanLogo} alt="Republican Candidate" width="40px" />
+                    <Text fontSize="large" fontWeight="bold" color="red.600">{republicanCandidatesCount} &nbsp;</Text>
+                    <Text color="red" fontWeight="bold"> Republican</Text>
+                </View>
+            </View>
+            <ComposableMap projection="geoAlbersUsa">
+                <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                        geographies.map((geo, index) => {
+                            // Convert state name to abbreviation
+                            const stateAbbreviation = stateAbbreviations[geo.properties.name];
+                            
+                            // Find the first candidate for this state
+                            const candidate = candidates.find(
+                                (candidate) => candidate.state === stateAbbreviation
+                            );
+                            const party = candidate ? candidate.party : null;
+    
+                            // Set color based on party
+                            let partyColor = "#DCDCDC"; // Default color if no candidates
+
+                            if (candidate) {
+                                if (party === "DEM") {
+                                    partyColor = "#0000FF"; // Blue for Democratic party
+                                } else if (party === "REP") {
+                                    partyColor = "#FF0000"; // Red for Republican party
+                                } else {
+                                    partyColor = "#778899"; // Color if candidates exist but not in parties
+                                }
+                            } else {
+                                partyColor = "#DCDCDC"; // Color if no candidates
+                            }
+                            
+    
+                            const isClicked = clickedCity === geo.properties.name;
+    
+                            return (
+                                <OverlayTrigger
+                                    key={index}
+                                    trigger="click"
+                                    rootClose
+                                    placement="right"
+                                    overlay={popover(geo)}
+                                >
+                                    <Geography
+                                        key={geo.rsmKey}
+                                        geography={geo}
+                                        fill={isClicked ? partyColor : "#C0C0C0"}
+                                        onClick={() => handleClick(geo)}
+                                        style={{
+                                            default: {
+                                                fill: partyColor,
+                                                outline: "#101010",
+                                                stroke: "#607D8B",
+                                                strokeWidth: 1,
+                                            },
+                                            hover: {
+                                                fill: partyColor,
+                                                outline: "#101010",
+                                                stroke: "#607D8B",
+                                                strokeWidth: 1,
+                                            },
+                                            pressed: {
+                                                outline: "#101010",
+                                                stroke: "#607D8B",
+                                                strokeWidth: 1,
+                                            },
+                                        }}
+                                    />
+                                </OverlayTrigger>
+                            );
+                        })
+                    }
+                </Geographies>
+            </ComposableMap>
       </div>
   );
   
--- src/pages/mapPage.jsx
+++ src/pages/mapPage.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { Grid, View, useTheme, SelectField , TextField} from '@aws-amplify/ui-react';
+import { Grid, View, useTheme, SelectField , TextField, Text, Image} from '@aws-amplify/ui-react';
 import MapChart from "../components/map";
 import '@aws-amplify/ui-react/styles.css';
+
+import democraticLogo from "../assets/logo/democraticLogo.png";
+import republicanLogo from "../assets/logo/republicanLogo.png";
 
 function MapPage() {
     const { tokens } = useTheme();
@@ -9,11 +12,30 @@ function MapPage() {
     const [electionCycle, setElectionCycle] = useState("2024");
     const [office, setOffice] = useState("S");
     const [searchTerm, setSearchTerm] = useState("");
+    const [searchType, setSearchType] = useState("name");
+    const [state, setState] = useState("");
+    const [party, setParty] = useState("");
 
     const officeNames = {
         S: "Senate",
         H: "House of Representatives",
         P: "Presidential" // Assuming 'P' is used for Presidential elections
+    };
+
+    const stateAbbreviations = {
+      "Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR",
+      "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE",
+      "Florida": "FL", "Georgia": "GA", "Hawaii": "HI", "Idaho": "ID",
+      "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS",
+      "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Maryland": "MD",
+      "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS",
+      "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV",
+      "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY",
+      "North Carolina": "NC", "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK",
+      "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI", "South Carolina": "SC",
+      "South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT",
+      "Vermont": "VT", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV",
+      "Wisconsin": "WI", "Wyoming": "WY"
     };
 
 
@@ -44,13 +66,13 @@ function MapPage() {
                         <View
                             padding={tokens.space.medium}
                             marginLeft={20}
+                            height="40%"
                             borderRadius="8px"
                             boxShadow="0 2px 4px rgba(0,0,0,0.1)"
                         >
                             <h4>Select Filters</h4>
                             <SelectField
                                 label="Select Election Cycle"
-                                placeholder="Election Cycle"
                                 marginBottom={tokens.space.small}
                                 onChange={(e) => setElectionCycle(e.target.value)}
                                 value={electionCycle}
@@ -79,8 +101,8 @@ function MapPage() {
                             alignItems="center"
                             height="100%"
                         >
-                            {/* Pass electionCycle and office as props */}
-                            <MapChart electionCycle={electionCycle} office={office} terms={searchTerm} />
+                            <MapChart electionCycle={electionCycle} office={office} terms={searchTerm} state={state} party={party} />
+                            
                         </View>
                         
                         {/* Right Column for additional information */}
@@ -89,15 +111,60 @@ function MapPage() {
                             borderRadius="8px"
                             boxShadow="0 2px 4px rgba(0,0,0,0.1)"
                             marginRight={20}
+                            height="40%"
                         >
-                            <h4>Search Candidate by Name</h4>
-                            <TextField
-                                label=""
-                                placeholder="Type name"
-                                value={searchTerm}
-                                onChange={(e) => setSearchTerm(e.target.value)}
-                                marginBottom={tokens.space.small}
-                            />
+                            <h4>Search Candidate</h4>
+                            
+                            {/* Radio buttons to select search type */}
+                            <div onChange={(e) => setSearchType(e.target.value)} style={{ display: 'flex', gap: '1rem', marginBottom: '1rem' }}>
+                                <div style={{ padding: '0.5rem', margin: '0.25rem' }}>
+                                    <label>
+                                        <input type="radio" value="name" name="searchType" defaultChecked onChange={ (e) => { setSearchTerm(""); setState(""); setParty(""); }} /> Name
+                                    </label>
+                                </div>
+                                <div style={{ padding: '0.5rem', margin: '0.25rem' }}>
+                                    <label>
+                                        <input type="radio" value="state" name="searchType" onChange={ (e) => { setSearchTerm(""); setState(""); setParty(""); }} /> State
+                                    </label>
+                                </div>
+                                <div style={{ padding: '0.5rem', margin: '0.25rem' }}>
+                                    <label>
+                                        <input type="radio" value="party" name="searchType" onChange={ (e) => { setSearchTerm(""); setState(""); setParty(""); }} /> Party
+                                    </label>
+                                </div>
+                            </div>
+
+
+                            {/* Conditional rendering based on selected search type */}
+                            {searchType === "name" && (
+                                <TextField
+                                    label=""
+                                    placeholder="Type name"
+                                    value={searchTerm}
+                                    onChange={(e) => {setSearchTerm(e.target.value);}}
+                                    marginBottom={tokens.space.small}
+                                />
+                            )}
+                            {searchType === "state" && (
+                                <SelectField
+                                    label=""
+                                    placeholder="Select state"
+                                    options={Object.keys(stateAbbreviations)} // Pass state names as strings
+                                    onChange={(e) => setState(stateAbbreviations[e.target.value])} // Map to abbreviation on selection
+                                    marginBottom={tokens.space.small}
+                                />
+                            )}
+                            {searchType === "party" && (
+                                <SelectField
+                                    label=""
+                                    placeholder="Select party"
+                                    onChange={(e) => setParty(e.target.value)}
+                                    marginBottom={tokens.space.small}
+                                >
+                                    <option value="DEM">Democratic</option>
+                                    <option value="REP">Republican</option>
+                                </SelectField>
+                            )}
                         </View>
                     </Grid>
                     {/* Party Legend Section */}
